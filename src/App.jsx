cd47da85bb29ed3860cd913a920200cf599c6925
Adi-31 <mail4adithya31@gmail.com>
2024-01-31 11:19:28 +0530

    initialized project

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import About from "./sections/About"
+import Features from "./sections/Features"
+import Footer from "./sections/Footer"
+import GetStarted from "./sections/GetStarted"
+import Hero from "./sections/Hero"
+import Pricing from "./sections/Pricing"
+
+function App() {
+
+  return (
+    <>
+     <Hero />
+     <About />
+     <Features />
+     <Pricing />
+     <GetStarted />
+     <Footer />
+    </>
+  )
+}
+
+export default App
---
+++ src/sections/Hero.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div className='bg-black text-white'>Machfolio</div>
+  )
+}
+
+export default Hero
